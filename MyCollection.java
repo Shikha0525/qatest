5e7728cb756e34d74702975a5292db619d6b63fd
Shikha0525 <40261074+Shikha0525@users.noreply.github.com>
2018-07-09 17:16:50 +0530

    Add files via upload

---
+++ MyCollection.java
@@ -0,0 +1,121 @@
+package question1;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Please implement Dynamic Array Interface 
+ * @author 
+ *
+ */
+public class MyCollection implements DynamicArray{
+// Uncomment commented section
+	
+	/**
+	 * You can declare local/global variables as per your requirement
+	 */
+	@SuppressWarnings("unused")
+	private int[] numArray;
+	int addedSoFar=0;
+	/**
+	 * It constructs an empty Collection object with an array capacity specified by the integer
+		parameter "arraySize".
+	 */
+	public MyCollection(int arraySize){
+		numArray = new int[arraySize];
+	}
+
+	public int search(int searchingNum) {
+		// TODO Auto-generated method stub
+		int x=searchingNum;
+		for(int i=0;i<numArray.length;i++)
+		{
+			if(numArray[i]==x)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public boolean add(int numberToAdd) 
+	{
+		// TODO Auto-generated method stub
+		if(search(numberToAdd)!=-1)
+			return false;
+		int count=getCount();
+		if(count==numArray.length)
+			doubleCapacity();
+		if(search(numberToAdd)==-1 && count<numArray.length)
+			
+			numArray[count]=numberToAdd;
+		addedSoFar++;
+			return true;
+	}
+
+	public void doubleCapacity() {
+		// TODO Auto-generated method stub
+		int[] newArray=new int[numArray.length*2];
+		for(int i=0;i<addedSoFar;i++)
+		{
+			newArray[i]=numArray[i];
+		}
+		numArray=newArray;
+	}
+
+	public boolean remove(int numberToRemove) {
+		// TODO Auto-generated method stub
+		int index=search(numberToRemove);
+		
+		if(index != -1)
+		{
+			for(int i=index;i<addedSoFar;i++)
+			{
+				
+				numArray[i]=numArray[i+1];
+			}
+			
+			addedSoFar--;
+			return true;
+		}
+		return false;
+	}
+
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return addedSoFar;
+	}
+
+	public int[] rotate(int n) {
+		// TODO Auto-generated method stub
+		
+		int k=0;
+		int arr[]= new int[n];
+		for(int i=0;i<n;i++)
+			arr[i]=numArray[i];
+		for(int i=0;i<addedSoFar-n;i++)
+			numArray[i]=numArray[i+n];
+		for(int i=(addedSoFar)-n;i<addedSoFar;i++)
+			numArray[i]=arr[k++];
+		
+		return numArray;
+	}
+	public String toString( )
+	{
+		String s="{";
+		if( addedSoFar!=0) 
+		{
+			for(int i=0;i< addedSoFar-1;i++)
+			{
+				s=s+numArray[i]+",";
+			}
+			s=s+numArray[ addedSoFar-1]+"}";
+		}
+		if( addedSoFar==0) {
+			return "{}";
+		}
+		return s;
+	}
+}
